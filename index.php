2d2d60c72ec85666f1d5a008f84675ac28ccf8e2
iamfrancisalbert <iamfrancisalbert@gmail.com>
2018-10-21 17:41:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
-  echo "<title> Francis Calas' Page </title>";
-  echo "<h1>Hello World! \n";
-  echo "Welcome to the page of Francis Albert Calas in the cloud!</h1> \n";
+  echo "<head><title> Francis Calas' Page </title></head>";
+  echo "<h1>Welcome to the page of Francis Albert Calas in the cloud!</h1> \n";
 
   phpinfo();
 
